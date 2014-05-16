7d389d977a1e21e3addd96a9256097e11448c2e8
anshulrouthu <anshul.m67@gmail.com>
2014-05-15 21:47:45 -0400

    Some minor changes

--- color-track.cpp
+++ color-track.cpp
@@ -1,10 +1,3 @@
-//============================================================================
-// Name        : opencv2.cpp
-// Author      : anshul
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
 
 #include <iostream>
 #include "opencv/cv.h"
